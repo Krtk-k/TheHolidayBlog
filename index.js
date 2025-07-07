d236a6deeba27bdf39240ce54042cb0f4e3cec96
Krtk-k <kartikkpandey05@gmail.com>
2025-07-07 14:12:04 +0530

    modified index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import bodyParser from "body-parser";
+const port = 3000;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Listning on port ${port}`);
+});
